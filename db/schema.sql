692302398d3a2ab2dcab328db3e6d6f6de5a1901
WhiteheadL <146836787+WhiteheadL@users.noreply.github.com>
2026-04-23 00:40:39 +0100

    SQL
    added schema.sql with users table definition

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
+--This File contains all the CREATE TABLE statments for the database
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEST NUL,
+    create_at TIMESTAMP NOT NULL DEFULT NOW()
+);
+
 
